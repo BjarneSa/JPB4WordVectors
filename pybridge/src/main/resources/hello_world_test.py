4490c7424b8ba5f5b71498fbad1ed09576c266fd
Bjarne Sauer <56190636+BjarneSa@users.noreply.github.com>
2020-03-20 13:13:11 +0100

    Update hello_world_test.py

--- pybridge/src/main/resources/hello_world_test.py
+++ pybridge/src/main/resources/hello_world_test.py
@@ -7,4 +7,20 @@ from configparser import RawConfigParser
 from flask import Flask, jsonify, request
 from array import array
 
+app = Flask(__name__) #server running on flask device
+
+dir_path = os.path.dirname(os.path.realpath(__file__)) #file path of this file
+
+config = configparser.RawConfigParser()
+configFilePath = os.path.join(dir_path, 'config.properties') #configFile should be saved in the same folder and named config.properties
+config.read(configFilePath)
+
+# Loading configuration from configuration file for url and port
+url_address = config['server']['ipAddress']
+port = config.getint('server', 'port')
+
+model = None
+wordVectors = None
+modelFileName = config['vectors']['modelFileName']
+
 print('Hello world')
