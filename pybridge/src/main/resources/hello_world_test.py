653406b8ccc166bc7e3de67cc7576a3835c874ac
Bjarne Sauer <56190636+BjarneSa@users.noreply.github.com>
2020-03-20 13:08:55 +0100

    Update hello_world_test.py

--- pybridge/src/main/resources/hello_world_test.py
+++ pybridge/src/main/resources/hello_world_test.py
@@ -1,1 +1,10 @@
+import io, os, sys, json
+import fasttext
+import configparser
+import socket
+import pytest
+from configparser import RawConfigParser
+from flask import Flask, jsonify, request
+from array import array
+
 print('Hello world')
