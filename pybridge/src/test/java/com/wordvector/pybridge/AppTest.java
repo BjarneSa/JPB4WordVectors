3754ef81b223b4d1bca0b5fa9f10670e04cab11e
Bjarne Sauer <56190636+BjarneSa@users.noreply.github.com>
2020-03-24 13:21:12 +0100

    Extending AppTest with adjustments on reduced model
    AppTest included tests only as comment blocks because the reduced learning model included less word vectors. Now, the amount of tests is equal to those that were used with the whole model. They cannot be the same because we get test failures if we would use the old tests on the reduced learning model. Please, write tests for larger model yourself.

--- pybridge/src/test/java/com/wordvector/pybridge/AppTest.java
+++ pybridge/src/test/java/com/wordvector/pybridge/AppTest.java
@@ -1,6 +1,10 @@
 package com.wordvector.pybridge;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -35,101 +39,105 @@ public class AppTest {
         pythonBridge.shutdownServer();
     }
 
+    /**
+     * Test on one single vector to word "the". Comparison on entry with index 0.
+     * "The" is one o the first entries in model from fasttext based on wikipedia.
+     */
     @Test
     public void testVector() {
         assertEquals(0.08969999849796295, pythonBridge.getVector("the").get().getVectorEntry(0), delta);
     }
 
-//    /**
-//     * Test on one single vector to word. Comparison on entry with index 0.
-//     */
-//    @Test
-//    public void testOneVectorFirstIndex() {
-//        assertEquals(-0.19200000166893005, pythonBridge.getVector("hello").get().getVectorEntry(0), delta);
-//    }
-//
-//    /**
-//     * Test on one single vector to word. Comparison on entry with irregular index.
-//     */
-//    @Test
-//    public void testOneVectorSomeIndex() {
-//        assertEquals(-0.11010000109672546, pythonBridge.getVector("dog").get().getVectorEntry(191), delta);
-//    }
-//
-//    /**
-//     * Test on one single vector to word. Comparison on entry with index 299.
-//     */
-//    @Test
-//    public void testOneVectorLastIndex() {
-//        assertEquals(0.048500001430511475, pythonBridge.getVector("cat").get().getVectorEntry(299), delta);
-//    }
-//
-//    /**
-//     * Test multiple requests.
-//     */
-//    @Test
-//    public void testGetSomeVectors() {
-//        assertEquals(0.10819999873638153, pythonBridge.getVector("king").get().getVectorEntry(0), delta);
-//        assertEquals(-0.0820000022649765, pythonBridge.getVector("man").get().getVectorEntry(1), delta);
-//        assertEquals(-0.08169999718666077, pythonBridge.getVector("!").get().getVectorEntry(2), delta);
-//        assertEquals(-0.16670000553131104, pythonBridge.getVector("boy").get().getVectorEntry(3), delta);
-//    }
-//
-//    /**
-//     * Test requests of special characters.
-//     */
-//    @Test
-//    public void testSpecialCharacters() {
-//        assertEquals(0.05000000074505806, pythonBridge.getVector("...").get().getVectorEntry(16), delta);
-//        assertEquals(-0.07689999788999557, pythonBridge.getVector("*").get().getVectorEntry(184), delta);
-//        assertEquals(0.010700000450015068, pythonBridge.getVector("]").get().getVectorEntry(279), delta);
-//    }
-//
-//    /**
-//     * Test requests of numbers.
-//     */
-//    @Test
-//    public void testVectorToNumbers() {
-//        assertEquals(-0.13740000128746033, pythonBridge.getVector("0").get().getVectorEntry(0), delta);
-//        assertEquals(0.0010000000474974513, pythonBridge.getVector("100").get().getVectorEntry(1), delta);
-//        assertEquals(-0.02930000051856041, pythonBridge.getVector("10000").get().getVectorEntry(2), delta);
-//        assertEquals(0.05920000001788139, pythonBridge.getVector("7.5").get().getVectorEntry(3), delta);
-//    }
-//
-//    /**
-//     * Test same requests twice and returned vector should be same object because of
-//     * link to database.
-//     */
-//    @Test
-//    public void testGetVectorToWordTwice() {
-//        Optional<WordVector> returnedVector = pythonBridge.getVector("queen");
-//        assertEquals(-0.19709999859333038, returnedVector.get().getVectorEntry(20), delta);
-//        assertEquals(returnedVector, pythonBridge.getVector("queen"));
-//    }
-//
-//    /**
-//     * Test vector length == 300
-//     */
-//    @Test
-//    public void testGetVectorOnLength() {
-//        assertEquals(300, pythonBridge.getVector("tree").get().getLength());
-//    }
-//
-//    /**
-//     * Test whether vector to not English word is null.
-//     */
-//    @Test
-//    public void testGetVectorToUnknownWord() {
-//        assertTrue(pythonBridge.getVector("qwert").isEmpty());
-//    }
-//
-//    /**
-//     * Test index that is out of bounds for vector of length 300.
-//     */
-//    @Test
-//    public void testIndexOutOfBound() {
-//        assertThrows(IndexOutOfBoundsException.class, () -> {
-//            pythonBridge.getVector("index").get().getVectorEntry(300);
-//        });
-//    }
+    /**
+     * Test on one single vector to word. Comparison on entry with index 0.
+     */
+    @Test
+    public void testOneVectorFirstIndex() {
+        assertEquals(0.11980000138282776, pythonBridge.getVector("Earth").get().getVectorEntry(0), delta);
+    }
+
+    /**
+     * Test on one single vector to word. Comparison on entry with irregular index.
+     */
+    @Test
+    public void testOneVectorSomeIndex() {
+        assertEquals(-0.11010000109672546, pythonBridge.getVector("dog").get().getVectorEntry(191), delta);
+    }
+
+    /**
+     * Test on one single vector to word. Comparison on entry with index 299.
+     */
+    @Test
+    public void testOneVectorLastIndex() {
+        assertEquals(0.048500001430511475, pythonBridge.getVector("cat").get().getVectorEntry(299), delta);
+    }
+
+    /**
+     * Test multiple requests.
+     */
+    @Test
+    public void testGetSomeVectors() {
+        assertEquals(0.10819999873638153, pythonBridge.getVector("king").get().getVectorEntry(0), delta);
+        assertEquals(-0.0820000022649765, pythonBridge.getVector("man").get().getVectorEntry(1), delta);
+        assertEquals(-0.08169999718666077, pythonBridge.getVector("!").get().getVectorEntry(2), delta);
+        assertEquals(-0.16670000553131104, pythonBridge.getVector("boy").get().getVectorEntry(3), delta);
+    }
+
+    /**
+     * Test requests of special characters.
+     */
+    @Test
+    public void testSpecialCharacters() {
+        assertEquals(0.05000000074505806, pythonBridge.getVector("...").get().getVectorEntry(16), delta);
+        assertEquals(-0.07689999788999557, pythonBridge.getVector("*").get().getVectorEntry(184), delta);
+        assertEquals(0.010700000450015068, pythonBridge.getVector("]").get().getVectorEntry(279), delta);
+    }
+
+    /**
+     * Test requests of numbers.
+     */
+    @Test
+    public void testVectorToNumbers() {
+        assertEquals(-0.13740000128746033, pythonBridge.getVector("0").get().getVectorEntry(0), delta);
+        assertEquals(0.0010000000474974513, pythonBridge.getVector("100").get().getVectorEntry(1), delta);
+        assertEquals(-0.06270000338554382, pythonBridge.getVector("42").get().getVectorEntry(2), delta);
+        assertEquals(-0.023800000548362732, pythonBridge.getVector("1.5").get().getVectorEntry(3), delta);
+    }
+
+    /**
+     * Test same requests twice and returned vector should be same object because of
+     * link to database.
+     */
+    @Test
+    public void testGetVectorToWordTwice() {
+        Optional<WordVector> returnedVector = pythonBridge.getVector("queen");
+        assertEquals(-0.19709999859333038, returnedVector.get().getVectorEntry(20), delta);
+        assertEquals(returnedVector, pythonBridge.getVector("queen"));
+    }
+
+    /**
+     * Test vector length == 300
+     */
+    @Test
+    public void testGetVectorOnLength() {
+        assertEquals(300, pythonBridge.getVector("tree").get().getLength());
+    }
+
+    /**
+     * Test whether vector to not English word is null.
+     */
+    @Test
+    public void testGetVectorToUnknownWord() {
+        assertTrue(pythonBridge.getVector("qwert").isEmpty());
+    }
+
+    /**
+     * Test index that is out of bounds for vector of length 300.
+     */
+    @Test
+    public void testIndexOutOfBound() {
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            pythonBridge.getVector("index").get().getVectorEntry(300);
+        });
+    }
 }
